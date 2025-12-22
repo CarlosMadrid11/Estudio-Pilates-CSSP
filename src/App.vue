093b1774d2a0d7971d08b38fef171608417810d7
Carlos Madrid <b4rc4drid@gmail.com>
2025-12-21 21:09:27 -0700

    Actualizamos la estructura del layout de la app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
-
 <template>
-  <router-view />
+  <div id="app" class="min-h-screen flex flex-col">
+    <!-- Navbar -->
+    <header>
+      <!-- Aquí va a ir el Navbar dinámico -->
+    </header>
+
+    <!-- Contenido principal -->
+    <main class="flex-1">
+      <RouterView />
+    </main>
+
+    <!-- Footer -->
+    <footer>
+      <!-- Footer reutilizable -->
+    </footer>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+/* Por ahora vacío */
+</style>
